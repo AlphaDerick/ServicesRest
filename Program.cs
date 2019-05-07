2d95f86314adbf3456151173bdafa75b5cb17dc4
AlphaDerick <israel.salinas.hdez@gmail.com>
2019-05-07 11:24:10 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServicesExtends.Models;
+using ServicesExtends;
+
+namespace TestServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get
+             var result = Directory.DIRECTORY_BOOK_BTC_MXN.ServiceGet<RootObject<PayloadOnly>>();
+             Console.WriteLine(result.Model.payload.ToString());
+            
+            //Post
+             var user = new Usuario("Vainilla", "fail");
+             var tem = user.ServicePost<Respuesta<InfoUsuarioModel>, Usuario>(Directory.DIRECTORY_LOGIN);
+
+
+             Console.Read();
+
+        }
+    }
+}
